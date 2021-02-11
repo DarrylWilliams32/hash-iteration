89a0bf7ad10c0be33e06cba564e98e7e59781a85
darryl_williams <darrylwilliams6@gmail.com>
2021-02-11 17:16:52 -0500

    commit tree a32c39735b0405a713089857f64f1dd3c2cad612

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,11 +1,5 @@
-# birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
-#   "Amanda" => 27
-# }
-
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  birthday_kids.each do |babies, ages|
+    puts "Happy Birthday #{babies}! You are now #{ages} years old!"
+  end
 end
-
-
--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,10 @@
-# passengers = {
-# suite_a: "Amanda Presley", 
-# suite_b: "Seymour Hoffman", 
-# suite_c: "Alfred Tennyson", 
-# suite_d: "Charlie Chaplin", 
-# suite_e: "Crumpet the Elf"
-# }
-
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+  passengers.each do |room, prize|
+    if room == :suite_a && prize.start_with?("A")
+      winner = prize
+    end
+  end
+  
+  winner 
 end
